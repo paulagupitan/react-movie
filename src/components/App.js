8e5ae631b06f7a162f9b1dd6ae9259d4f8965949
judygong15 <judygong@hotmail.com>
2022-01-05 15:48:35 -0800

    initial files

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
