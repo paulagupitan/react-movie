0be324df2a712ac36e4c35d81142ad7d5f1e52f5
Paul Agupitan <paulagupitan@gmail.com>
2022-01-12 14:50:22 -0800

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-     
+
       </header>
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,14 @@
+import { getYear } from '../utilities/getDates';
+
+const Footer = ({ copyright, author }) => (
+    <footer>
+        <p>&copy; {copyright} {author}</p>
+    </footer>
+);
+
+Footer.defaultProps = {
+    author: 'Awesome Corp.',
+    copyright: getYear()
+}
+
 export default Footer;
