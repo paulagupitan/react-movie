533c939e5961e08d301f898934b1862e73a019bc
Megan Cheung <cheung.megann@gmail.com>
2022-01-12 14:10:57 -0800

    added global variables

---
+++ src/globals/globalVariables.js
@@ -0,0 +1,5 @@
+export const appTitle = '';
+export const appAuthor = '';
+export const endPointSearch = '';
+export const endPointAllMovies = '';
+export const apiKey = '';
--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -1,1 +1,6 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { appTitle, endPointSearch, endPointAllMovies, apiKey } from '../globals/globalVariables';
+
+
+
+export default PageHome;
