6bc0739359fd363e9d928708755147f03946c710
Paul Agupitan <paulagupitan@gmail.com>
2022-01-12 14:24:44 -0800

    changed

--- src/pages/PageContact.js
+++ src/pages/PageContact.js
@@ -1,3 +1,14 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+<<<<<<< Updated upstream
 export default PageContact;
+=======
+
+function PageContact() {
+
+
+}
+
+
+export default PageContact;
+>>>>>>> Stashed changes
--- src/pages/PageFavourites.js
+++ src/pages/PageFavourites.js
@@ -1,3 +1,13 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+<<<<<<< Updated upstream
 export default PageFavourties;
+=======
+
+function PageFavourites() {
+
+}
+
+
+export default PageFavourites;
+>>>>>>> Stashed changes
