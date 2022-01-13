51a45c55eaf59cf6be5de3909f4ffb3e1ea6f7ba
Paul Agupitan <paulagupitan@gmail.com>
2022-01-12 21:54:48 -0800

    updated

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -3,6 +3,15 @@
 
 function PageAbout() {
 
+    return (
+        <main>
+            <section>
+                <h2>About this App</h2>
+                <p>This app was created with love ❤️ by Snowball. 😎...</p>
+            </section>
+        </main>
+    );
+
 }
 
 
