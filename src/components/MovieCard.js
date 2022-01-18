4a810b2fe5307e0606fee3e072c55376eb154ec2
Megan Cheung <cheung.megann@gmail.com>
2022-01-18 10:35:17 -0800

    update the movie info

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -11,16 +11,15 @@ function MovieCard({ movie }) {
         <div className="movie-card">
             <div className="movie-poster">
                 <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
-
-                <div className="movie-info">
-
-                    <h3 className="movie-date">{dateFormat(movie.release_date)}</h3>
-                    <h3 className="movie-rating">{movie.vote_average}</h3>
-                    <h2>{movie.title}</h2>
-                    <p className="movie-overview">{movie.overview}</p>
-                    <div className="more-info-btn-container">
-                        <Link to={`/movie/${movie.id}`}><button className="more-info-btn">More Info</button></Link>
-
+                <div className="overlay">
+                    <div className="movie-info">
+                        <h3 className="movie-date">{dateFormat(movie.release_date)}</h3>
+                        <h3 className="movie-rating">{movie.vote_average}</h3>
+                        <h2>{movie.title}</h2>
+                        <p className="movie-overview">{movie.overview}</p>
+                        <div className="more-info-btn-container">
+                            <Link to={`/movie/${movie.id}`}><button className="more-info-btn">More Info</button></Link>
+                        </div>
                     </div>
                 </div>
             </div>
