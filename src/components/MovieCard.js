ac69f79000ddf760ce137dc7b1b0c7461ea69f51
Megan Cheung <cheung.megann@gmail.com>
2022-01-17 19:18:49 -0800

    fixed the info

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,20 +1,32 @@
 import { Link } from 'react-router-dom';
 
-function MovieCard({movie}) {
+function MovieCard({ movie }) {
+
+    function dateFormat(string) {
+        let date = { year: 'numeric', month: 'long', day: 'numeric' };
+        return new Date(string).toLocaleDateString([], date);
+    }
+
+    function timeFormat(string) {
+        let time = { hour: 'numeric', minute: 'numeric' };
+        return new Date(string).toLocaleDateString([], time);
+    }
     return (
         <div className="movie-card">
             <div className="movie-poster">
-            <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
+                <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
             </div>
             <div className="movie-info">
-                <p>{movie.title}</p>
-                <p>{movie.release_date}</p>
-                <p>{movie.vote_average}</p>
+                <h2>{movie.title}</h2>
+                <h3>{dateFormat(movie.release_date)}</h3>
+                <h3>{movie.vote_average}</h3>
                 <p>{movie.overview}</p>
-                <Link to={`/movie/${movie.id}`}>More Info</Link>
+                <div className="more-info-btn">
+                    <Link to={`/movie/${movie.id}`}>More Info</Link>
+                </div>
             </div>
 
-            
+
         </div>
     )
 }
