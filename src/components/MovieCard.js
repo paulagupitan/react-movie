0af337de58297d8595565df0aeb5522faf6a5f9d
Megan Cheung <cheung.megann@gmail.com>
2022-01-17 21:12:39 -0800

    added hover to posters

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,26 +3,25 @@ import { Link } from 'react-router-dom';
 function MovieCard({ movie }) {
 
     function dateFormat(string) {
-        let date = { year: 'numeric', month: 'long', day: 'numeric' };
+        const date = { year: 'numeric', month: 'long', day: 'numeric' };
         return new Date(string).toLocaleDateString([], date);
     }
 
-    function timeFormat(string) {
-        let time = { hour: 'numeric', minute: 'numeric' };
-        return new Date(string).toLocaleDateString([], time);
-    }
     return (
         <div className="movie-card">
             <div className="movie-poster">
                 <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
-            </div>
-            <div className="movie-info">
-                <h2>{movie.title}</h2>
-                <h3>{dateFormat(movie.release_date)}</h3>
-                <h3>{movie.vote_average}</h3>
-                <p>{movie.overview}</p>
-                <div className="more-info-btn">
-                    <Link to={`/movie/${movie.id}`}>More Info</Link>
+
+                <div className="movie-info">
+
+                    <h3 className="movie-date">{dateFormat(movie.release_date)}</h3>
+                    <h3 className="movie-rating">{movie.vote_average}</h3>
+                    <h2>{movie.title}</h2>
+                    <p className="movie-overview">{movie.overview}</p>
+                    <div className="more-info-btn-container">
+                        <Link to={`/movie/${movie.id}`}><button className="more-info-btn">More Info</button></Link>
+
+                    </div>
                 </div>
             </div>
 
