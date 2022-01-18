46845e9a82144a5827ca469e0da100a7e5f7c9d2
Paul Agupitan <paulagupitan@gmail.com>
2022-01-17 23:02:58 -0800

    updated about and fave scss and content as well

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -5,9 +5,12 @@ function PageAbout() {
 
     return (
         <main>
-            <section>
-                <h2>About this App</h2>
-                <p>This app was created with love ❤️ by Snowball. 😎...</p>
+            <section className="about-page">
+                <h2>Let's talk about XTRAMOV!E </h2>
+                <p>The XTRAMOV!E movie database is a website where users can find popular, top rated, now playing and upcoming movie listings.</p>
+                <p>Browse for your favourite genres or movies, check out their ratings and see how they match up!</p>
+                <p>Found something you like? Add them to your favourites and remove any item anytime.</p>
+                <p>This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
             </section>
         </main>
     );
