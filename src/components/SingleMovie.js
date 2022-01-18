9c26ee6f7134d8e68c526db4cf687f326b2aac71
judygong15 <judygong@hotmail.com>
2022-01-18 09:55:23 -0800

    styled single movie

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -2,24 +2,28 @@ import FavButton from "./FavButton";
 
 function SingleMovie({movie}) {
     return (
-        <div className="movie-card">
+        <div className="movie-card-single">
             <div className="movie-backdrop">
-            <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title} />
-            </div>
-            <div className="movie-poster">
-                <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
-            </div>
-            <div className="movie-info">
-                <p>{movie.release_date}</p>
-                <p>{movie.runtime}</p>
-                <p>{movie.vote_average}</p>
-                <h2>{movie.title}</h2>
-                <p>{movie.overview}</p>
-                <p>Genre: {movie.genres[0].name}</p>
-                <FavButton />
-            </div>
+                <img className="backdrop-img" src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title} />
+                <img className="poster-mobile" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
 
-            
+            </div>
+       
+            <div className="movie-single">
+                <img className="poster-desktop" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
+
+                <div className="movie-info">
+                    <div className="release-runtime-vote">
+                        <p>{movie.release_date}</p>
+                        <p>{movie.runtime}</p>
+                        <p className="vote">{movie.vote_average}</p>
+                    </div>
+                    <h2>{movie.title}</h2>
+                    <p>{movie.overview}</p>
+                    <p>Genre: {movie.genres[0].name}</p>
+                    <FavButton />
+                </div>      
+            </div>
         </div>
     )
 }
