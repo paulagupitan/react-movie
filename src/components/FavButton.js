e70357272ae2f97ede94e79276ec08f2530f362a
Megan Cheung <cheung.megann@gmail.com>
2022-01-21 16:35:01 -0800

    update favbutton

--- src/components/FavButton.js
+++ src/components/FavButton.js
@@ -4,7 +4,7 @@ import { isFav, addToFavs, removeFromFavs } from '../utilities/storage';
 function FavButton({ movie }) {
 
     const [fav, setFav] = useState(isFav(movie));
-
+    const [favList, setFavList] = useState();
     let handleAddToFavs = () => {
         setFav(true);
         addToFavs(movie);
@@ -17,7 +17,6 @@ function FavButton({ movie }) {
 
     return (
         <div>
-
             {fav === false ?
                 <button className="fav-button" onClick={handleAddToFavs}>Add to Favourites</button> :
                 <button className="fav-button" onClick={handleRemoveFromFavs}>Remove from Favourites</button>
