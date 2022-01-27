9b4553cb97b167201abce86bfe807cabce399d71
Megan Cheung <cheung.megann@gmail.com>
2022-01-27 15:26:02 -0800

    fixed the height of poster

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -20,14 +20,13 @@ function MovieCard({ movie }) {
                     <h3 className="movie-date">{dateFormat(movie.release_date)}</h3>
                     <h3 className="movie-rating">{movie.vote_average}</h3>
                     <h2>{movie.title}</h2>
-
                     <p className="movie-overview">{movie.overview}</p>
-
                     <Link to={`/movie/${movie.id}`} className="more-info">More Info</Link>
                 </div>
             </div>
         </div>
     )
 }
+
 
 export default MovieCard;
