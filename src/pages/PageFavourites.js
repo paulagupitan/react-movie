2f4f33e95b34eaaa4df4d43ef08b2b1d647b9a08
Paul Agupitan <paulagupitan@gmail.com>
2022-01-26 23:40:18 -0800

    Done with nav

--- src/pages/PageFavourites.js
+++ src/pages/PageFavourites.js
@@ -31,7 +31,7 @@ function PageFavourites({ favourites, setFavourites }) {
         <main>
             <section className="fav-page">
 
-                {favourites.length > 0 ? favourites.map(movie => <FavCard favourites={favourites} setFavourites={setFavourites} key={movie.id} movie={movie} />) : <div className="fav-page-info"><h3>No movies have been added to favourites.</h3><p>To add a movie to your favourites, click on the Movie Title or hover over a movie poster to go to that specific movie's description page.</p></div>}
+                {favourites.length > 0 ? favourites.map(movie => <FavCard favourites={favourites} setFavourites={setFavourites} key={movie.id} movie={movie} />) : <div className="fav-page-message"><h3>No movies have been added to favourites.</h3><p>To add a movie to your favourites, click on the Movie Title or hover over a movie poster to go to that specific movie's description page.</p></div>}
 
             </section>
         </main >
