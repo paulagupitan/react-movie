ceb51c680f22ecefe0b4e30d9bd2589860f8f36e
Paul Agupitan <paulagupitan@gmail.com>
2022-01-29 23:03:36 -0800

    updated no poster version 2

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -25,7 +25,7 @@ function SingleMovie({ movie, favourites, setFavourites }) {
             <div className="movie-backdrop">
                 {movie.backdrop_path === null ? <img className="no-backdrop" src={noDisplay} alt="No Backdrop Poster" /> : <img className="backdrop-img" src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title} />}
                 {movie.poster_path === null ?
-                    <img className="poster-mobile" src={noPoster} alt="No poster available." /> :
+                    <img className="no-poster" src={noPoster} alt="No poster available." /> :
                     <img className="poster-mobile" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />}
 
             </div>
