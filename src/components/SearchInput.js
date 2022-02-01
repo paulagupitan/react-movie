63710224e0662c3dfe2206cecc87fe221224c99c
judygong15 <judygong@hotmail.com>
2022-01-31 17:14:26 -0800

    fixing search on deployed version

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,12 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+
 function SearchInput({ query }) {
+
+    let navigate = useNavigate();
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        let q = e.target.elements.search.value;
+
+        navigate(`/search?search=${q}`, { replace: true });
+
+    }
 
     return (
         <div className="search">
-            <form className="search-form" action="/search" method="get">
-                <label className="sr-only">Search</label>
+            <form className="search-form" action="/search" method="get" onSubmit={handleSubmit}>
+                <label className="sr-only" for="search">Search</label>
                 <input className='search-bar'
                     type="text"
                     name="search"
+                    id="search"
                     value={query}
                     placeholder='Search...' />
             </form>
